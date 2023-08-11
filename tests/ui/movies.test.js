d299b41f47a4af7fb31e4939eb1c37cb503a0fff
Evgeni <evgeni.s@students.softuni.bg>
2023-08-11 18:40:08 +0100

    fixed test bug

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-  await page.goto('https://my-soft-uni-username.onrender.com/');
+  await page.goto('https://my-soft-uni-username.onrender.com/movies');
   const list = await page.$('ul');
   expect(list).toBeTruthy();
 });
